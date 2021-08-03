770c9f3fb945572e0d3da477fcb136ad154881e9
fschweikhardt <schweikhardt.web@gmail.com>
2021-08-03 12:35:10 -0400

    added test

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,42 @@
+<refrence types="cypress"/>
+
+describe("Our first test suite", () => {
+
+    it("should be a test", () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //by tag name
+        cy.get('input')
+
+        //by id
+        cy.get('#inputEmail')
+
+        //by clss name
+        cy.get('.input-full-width')
+
+        //by attribute name
+        cy.get('[placeholder]')
+
+        //by attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        //by class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        //by tag name
+        cy.get('input[placeholder="Email"]')
+
+        //two different attributes
+        cy.get('[placeholder="Email"][type="Email"]')
+
+        //by tag name, attribute value, id and class name
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        //recommended
+        cy.get('[data-cy="imputEmail1"]')
+    })
+     
+})
