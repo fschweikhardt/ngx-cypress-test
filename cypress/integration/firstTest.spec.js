0a0748a43661b8d3c3c32c0099ec52e55ebddfcd
fschweikhardt <schweikhardt.web@gmail.com>
2021-08-04 16:31:58 -0400

    lists and dropdowns

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -148,7 +148,7 @@ describe("Our first test suite", () => {
 
     })
 
-    it.only('checkboxes', () => {
+    it('checkboxes', () => {
 
         cy.contains('Modal').click()
         cy.contains('Toastr').click()
@@ -156,5 +156,39 @@ describe("Our first test suite", () => {
         cy.get('[type="checkbox"]').check({force: true})
         cy.get('[type="checkbox"]').eq(1).click({force: true})
 
+    })
+
+    it.only('lists and dropdowns', () => {
+        
+        //method 1
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').click().should('contain', 'Dark')
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+        //method 2
+
+        cy.get('nav nb-select').click().then(dropdown => {
+            cy.wrap(dropdown).click()
+            cy.get('.options-list nb-option').each( (listItem, index) => {
+                
+                const itemText = listItem.text().trim()
+
+                const colors = {
+                    Light : "rgb(255, 255, 255)",
+                    Dark : "rgb(34, 43, 69)",
+                    Cosmic : "rgb(50, 50, 89)",
+                    Corporate : "rgb(255, 255, 255)"
+                }
+
+                cy.wrap(listItem).click()
+                cy.wrap(dropdown).should('contain', itemText)
+                cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+                if (index < 3) {
+                    cy.wrap(dropdown).click()
+                }
+
+            })
+        })
     })
 })
